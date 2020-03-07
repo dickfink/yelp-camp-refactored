e0fd5d99e7c7216cb5b4c6fee5b869894155e55a
Dick Fink <dickfink@gmail.com>
2020-03-07 14:58:52 -0800

    show user's campgrounds on profile
    Seems "cheap" need to figure out how to actually use Mongoose to search by ID.

--- routes/profile.js
+++ routes/profile.js
@@ -12,34 +12,40 @@ function escapeRegex(text) {
 
 //INDEX - show all campgrounds
 router.get("/", isLoggedIn, function(req, res){
-  if(req.query.search && req.xhr) {
-      const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+//   if(req.query.search && req.xhr) {
+//       const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+//       // Get all campgrounds from DB
+//       Campground.find({name: regex}, function(err, allCampgrounds){
+//          if(err){
+//             console.log(err);
+//          } else {
+//             res.status(200).json(allCampgrounds);
+//          }
+//       });
+//   } else {
+   var currentUser = res.locals.currentUser;
+   console.log("Current user: " + currentUser.username);
       // Get all campgrounds from DB
-      Campground.find({name: regex}, function(err, allCampgrounds){
-         if(err){
-            console.log(err);
-         } else {
-            res.status(200).json(allCampgrounds);
-         }
-      });
-  } else {
-   var currentUser = res.locals.currentUser._id;
-   console.log(currentUser);
-      // Get all campgrounds from DB
-      Campground.find({}, function(err, allCampgrounds){
+      Campground.find({}).exec(function(err, allCampgrounds){
          if(err){
              console.log(err);
          } else {
             if(req.xhr) {
               res.json(allCampgrounds);
             } else {
-            console.log(allCampgrounds);
-              res.render("profile",{campgrounds: allCampgrounds, page: 'profile'});
+               let userCampgrounds = [];
+            allCampgrounds.forEach(campground => {
+               console.log("Campground User: " + campground.author.username)
+               if(currentUser.username == campground.author.username)
+                  {userCampgrounds.push(campground)}; 
+            });
+            console.log("all user's campgrounds: ")
+            console.log(userCampgrounds);
+            res.render("profile",{campgrounds: userCampgrounds, page: 'profile'});
             }
          }
       });
-  }
-});
+  });
 
 module.exports = router;
 
